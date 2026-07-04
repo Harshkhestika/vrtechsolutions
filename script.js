43af1dd77c012b04f4961273cd95333a6dd4779b
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-07-04 23:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentSlideIndex = {};
 let autoSlideIntervals = {};
 let isUserInteracting = {} ;
 
-
+ 
 // Initialize the website...
 document.addEventListener('DOMContentLoaded', function () {
     initializeSliders();
